3e54ec558e446674ee0d816f5b3c578f6d0f6f44
Dao Quang Phuong <daoquangphuong@gmail.com>
2020-10-28 19:33:09 +0700

    fix storage name

--- src/commands/fix.js
+++ src/commands/fix.js
@@ -21,16 +21,55 @@ const fix = workingPath => {
       }
 
       const fileContent = fs.readFileSync(itemPath, 'utf8');
+
+      const currentDir = path.basename(where);
+
+      const storageNameMatch = fileContent.match(/createStore\((.+?)\)/);
+      let newFileContent = fileContent;
+
+      if (storageNameMatch && currentDir === 'stores') {
+        const nameMatchList = newFileContent.match(
+          /const\s+\$(.+?)\s+=\s+createStore\((.+?)\)/g
+        );
+
+        const parentDirName = path.basename(path.resolve(where, '../'));
+
+        nameMatchList.forEach(text => {
+          const nameMatch = text.match(
+            /const\s+\$(.+?)\s+=\s+createStore\((.+?)\)/
+          );
+
+          const moduleName =
+            parentDirName[0] === '@'
+              ? parentDirName.substring(1)
+              : parentDirName;
+
+          const firstComma = nameMatch[2][0];
+          const lastComma = nameMatch[2][nameMatch[2].length - 1];
+
+          const storeName = `${firstComma}${moduleName}:${nameMatch[1]}${lastComma}`;
+
+          if (storeName !== nameMatch[2]) {
+            newFileContent = newFileContent.replace(nameMatch[2], storeName);
+          }
+        });
+
+        if (newFileContent !== fileContent) {
+          fs.writeFileSync(itemPath, newFileContent, 'utf8');
+          shell.exec(`git add ${itemPath.replace(' ', '\\ ')}`);
+        }
+
+        return;
+      }
       const componentNameMatch = fileContent.match(/container\((.+?)\)/);
       if (!componentNameMatch) {
         return;
       }
       const componentName = componentNameMatch[1];
-      const currentDir = path.basename(where);
       if (componentName === currentDir) {
         return;
       }
-      const newFileContent = fileContent.replace(
+      newFileContent = fileContent.replace(
         new RegExp(`${componentName}`, 'g'),
         currentDir
       );
